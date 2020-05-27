6816c9a9aa332d3f8ff4d26c1dc9dca349185e84
Shereece Victor <alexreecevictor@gmail.com>
2020-05-27 13:12:03 -0400

    Delete works

--- main.py
+++ main.py
@@ -14,19 +14,21 @@ login_manager = LoginManager()
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
+
 ''' End Flask Login Functions '''
 
 
 def create_app():
-	app = Flask(__name__, static_url_path='')
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-	app.config['SECRET_KEY'] = "MYSECRET"
-	#   app.config['JWT_EXPIRATION_DELTA'] = timedelta(days = 7) # uncomment if using flsk jwt
-	CORS(app)
-	login_manager.init_app(app)  # uncomment if using flask login
-	db.init_app(app)
-	return app
+    app = Flask(__name__, static_url_path='')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config['SECRET_KEY'] = "MYSECRET"
+    #   app.config['JWT_EXPIRATION_DELTA'] = timedelta(days = 7) # uncomment if using flsk jwt
+    CORS(app)
+    login_manager.init_app(app)  # uncomment if using flask login
+    db.init_app(app)
+    return app
 
 
 app = create_app()
@@ -38,56 +40,58 @@ app.app_context().push()
 
 @app.route('/')
 def login():
-	if request.method == 'GET':
-		return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
 
 
 @app.route('/auth', methods=['POST'])
 def authenticate():
-	data = request.form.to_dict()
-	usernme = data['username']
-	password = data['password']
-	user = User.query.filter_by(username=usernme).first()
-	if user and user.check_password(password):
-		flash('Login successful')
-		login_user(user)
-		return redirect(url_for('loadhome'))
-	flash('Invalid credentials')
-	return redirect(url_for('login'))
+    data = request.form.to_dict()
+    usernme = data['username']
+    password = data['password']
+    user = User.query.filter_by(username=usernme).first()
+    if user and user.check_password(password):
+        flash('Login successful')
+        login_user(user)
+        return redirect(url_for('loadhome'))
+    flash('Invalid credentials')
+    return redirect(url_for('login'))
 
 
 @app.route('/myfriendspace', methods=['POST'])
 @login_required
 def createpost():
-	newpost = request.form.to_dict()
-	u_id = current_user.id
-	content = newpost['posttext']
-	addpost = Post(userId=u_id, text=content)
-	try:
-		db.session.add(addpost)
-		db.session.commit()
-		return redirect(url_for('loadhome'))
-	except IntegrityError:
-		db.session.rollback()
-		return "Routine already exists", 400
+    newpost = request.form.to_dict()
+    u_id = current_user.id
+    content = newpost['posttext']
+    addpost = Post(userId=u_id, text=content)
+    try:
+        db.session.add(addpost)
+        db.session.commit()
+        return redirect(url_for('loadhome'))
+    except IntegrityError:
+        db.session.rollback()
+        return "Routine already exists", 400
 
 
 @app.route('/myfriendspace', methods=['GET'])
 @login_required
 def loadhome():
-	posts = Post.query.all()
-	posts = [p.toDict() for p in posts]
-	return render_template("app.html", posts=posts)
+    posts = Post.query.all()
+    posts = [p.toDict() for p in posts]
+    return render_template("app.html", posts=posts)
 
-@app.route('/myfriendspace', methods=['DELETE'])
-login_required()
-def deletepost():
-  post = Post.query.filter_by(userId=current_user.id, id=pid)
-  poke = MyPokemon.query.filter_by(id=current_identity.id, bid=b_id).first()
-  if poke == None:
-    return 'Can\'t delete a pokemon that doesn\'t exist'
-  db.session.delete(poke)
-  db.session.commit()
-  return 'Deleted', 204
+
+@app.route('/myfriendspace/delete/<pid>', methods=['POST'])
+@login_required
+def deletepost(pid):
+    print(pid)
+    post = Post.query.filter_by(id=pid).first()
+    print(post)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('loadhome'))
+
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- models.py
+++ models.py
@@ -1,67 +1,70 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+
 db = SQLAlchemy()
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(50), nullable=False)
+	email = db.Column(db.String(120), unique=True, nullable=False)
+	password = db.Column(db.String(50), nullable=False)
 
-    def toDict(self):
-        return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email,
-            'password': self.password
-        }
+	def toDict(self):
+		return {
+			'id': self.id,
+			'username': self.username,
+			'email': self.email,
+			'password': self.password
+		}
 
-    def set_password(self, password):
-        self.password = generate_password_hash(password, method='sha256')
+	def set_password(self, password):
+		self.password = generate_password_hash(password, method='sha256')
 
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+	def check_password(self, password):
+		return check_password_hash(self.password, password)
 
 
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, db.ForeignKey('user.id'))
-    text = db.Column(db.String(280), nullable=False)  # Same length as twitter
-    reacts = db.relationship('UserReact')
+	id = db.Column(db.Integer, primary_key=True)
+	userId = db.Column(db.Integer, db.ForeignKey('user.id'))
+	text = db.Column(db.String(280), nullable=False)  # Same length as twitter
+	reacts = db.relationship('UserReact', backref='user', lazy=True, cascade="all, delete-orphan")
 
+	def getTotalLikes(self):  # TODO: Check on which user likes you counting
+		numLikes = 0
+		for r in self.reacts:
+			if r.postId == self.id:
+				if r.react == "like":
+					numLikes += 1
+		return numLikes
 
-    def getTotalLikes(self):
-        numLikes = 0
-        for r in self.reacts:
-            if r == "like":
-                numLikes += 1
-        return numLikes
+	def getTotalDislikes(self):
+		numDislikes = 0
+		for r2 in self.reacts:
+			if r2.postId == self.id:
+				if r2 == "dislike":
+					numDislikes += 1
+		return numDislikes
 
-    def getTotalDislikes(self):
-        numDislikes = 0
-        for r2 in self.reacts:
-            if r2 == "dislike":
-                numDislikes += 1
-        return numDislikes
+	def getusername(self):
+		name = db.session.query(User.username).filter_by(id=self.userId).first()
+		name = name[0]
+		return name
 
-    def getusername(self):
-        name = db.session.query(User.username).filter_by(id=self.userId).first()
-        name = name[0]
-        return name
-
-    def toDict(self):
-        return {
-            'userId': self.userId,
-            'username': self.getusername(),  # TODO: somehow fetch username
-            'text': self.text,
-            'likes': self.getTotalLikes(),
-            'dislikes': self.getTotalDislikes()
-        }
+	def toDict(self):
+		return {
+			'id': self.id,
+			'userId': self.userId,
+			'username': self.getusername(),
+			'text': self.text,
+			'likes': self.getTotalLikes(),
+			'dislikes': self.getTotalDislikes()
+		}
 
 
 class UserReact(db.Model):
-    userId = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    postId = db.Column(db.Integer, db.ForeignKey('post.id'), primary_key=True)
-    react = db.Column(db.String(10), nullable=False)
+	userId = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+	postId = db.Column(db.Integer, db.ForeignKey('post.id'), primary_key=True)
+	react = db.Column(db.String(10), nullable=False)
